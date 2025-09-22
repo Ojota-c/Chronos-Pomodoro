6155b4b674b31f0e271bd057f7820fe3127fd8f2
joao <joaocb202@gmail.com>
2025-09-22 18:27:40 -0300

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
-import styles from './styles.module.css';
-
-type HeadingProps = {
-  children: React.ReactNode;
-};
-
-export function Heading({ children }: HeadingProps) {
-  return <h1 className={styles.heading}>{children}</h1>;
+export function Heading() {
+  return (
+    <footer>
+      <a href=''>Entenda como funciona a tecnica Pomodoro</a>
+    </footer>
+  );
 }
