44bb987c1fa56964232387766a87ca3d1554e93f
joao <joaocb202@gmail.com>
2025-12-05 07:08:07 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 //* Pages
-import { useState } from 'react';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { Home } from './pages/Home';
 
 //* Styles
 import './styles/global.css';
 import './styles/theme.css';
-import type { TaskStateModel } from './models/TaskStateModel';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle:0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 15,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
 
-  console.log('App', state)
-
-  return <Home state={state} setState={setState}/>;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,31 +1,43 @@
-import { PlayCircleIcon } from "lucide-react";
-import { Cycles } from "../Cycles";
-import { DefaultButton } from "../DefaultButton";
-import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import { useRef, useState } from 'react';
 
 export function MainForm() {
-    return (
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite sua tarefa'
-            />
-          </div>
+  const [taskName, setTaskName] = useState('');
+  const numero = useRef(0)
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+    numero.current += 1 
+    console.log('João Carlos',taskName);
+  }
 
-          <div className='formRow'>
-            <Cycles />
-          </div>
+  return (
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite sua tarefa'
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
+        />
+      </div>
 
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
-    )
+      <div className='formRow'>
+        <p>Próximo intervalo e de 25min</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} color='green' />
+      </div>
+    </form>
+  );
 }
